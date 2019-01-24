2d738d287ab1509d23a916c8cbbae47981bc2089
cloudstrife9494 <aymansmimah@yahoo.fr>
2019-01-24 21:16:03 +0100

    integrate slider component

---
+++ src/components/gameForm/Slider.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import 'nouislider';
+import '../../../assets/css/nouislider.min.css';
+
+
+export default class Slider extends React.Component {
+    state={
+        currentValue: 0
+    }
+
+    componentDidMount(){
+        const {onValueUpdate, name,maxValue}= this.props
+        
+        var slider= document.getElementById(`slider-container-${name}`)
+        noUiSlider.create(slider ,{
+            start: [this.state.currentValue],
+            connect: true,
+            range: {
+                'min': 0,
+                'max': maxValue || 100
+            } 
+        })
+
+        slider.noUiSlider.on('update',(value)=>{
+            this.setState({currentValue: parseInt(value[0]) },()=> onValueUpdate(this.state.currentValue) )
+        })
+
+         
+    }
+
+    render(){
+        const {name}= this.props
+        return (
+            <div>
+               <div id={`slider-container-${name}`} ></div>
+               <div className="slider-current-value">{this.state.currentValue}</div>
+            </div>
+        )
+    }
+}
